c5ba771f999360a8027049768ff0b27f622fa041
Kirit Sælensminde <k@kirit.com>
2013-05-28 11:55:07 +0700

    Bumped version number

--- setup.py
+++ setup.py
@@ -10,7 +10,7 @@ def read(fname1, fname2):
 
 setup(
     name = "mkschroot",
-    version = "0.2.4",
+    version = "0.3",
     author = "Kirit Saelensminde",
     author_email = "kirit@felspar.com",
     url='https://github.com/KayEss/mkschroot',
